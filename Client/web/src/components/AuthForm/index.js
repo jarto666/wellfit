23508d167df04b9985722be63c4b7f4bc9bd5ae2
Artem Korytov <a.korytov@sitt.me>
2020-05-04 00:06:53 +0300

    simple log in form

--- Client/web/src/components/AuthForm/index.js
+++ Client/web/src/components/AuthForm/index.js
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './index.module.css';
 
-const AuthForm = () => (
-  <div className={styles.wrapper}>
-    <form className={styles.form}>
-      <label>
-        <input className={styles.input} type="text" placeholder="login" />
-      </label>
+const AuthForm = () => {
+  const [isRegistration, setIsRegistration] = useState(false);
 
-      <label>
-        <input className={styles.input} type="text" placeholder="password" />
-      </label>
-    </form>
-  </div>
-);
+  return (
+    <div className={styles.wrapper}>
+      <form className={styles.form}>
+        <label>
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="Login"
+          />
+        </label>
+
+        <label>
+          <input
+            className={styles.input}
+            type="password"
+            placeholder="Password"
+          />
+        </label>
+
+        <button
+          className={styles.button}
+          type="submit"
+          onClick={(e) => { e.preventDefault(); }}
+        >
+          Log in
+        </button>
+
+        <span className={styles.formLabel}>
+          {isRegistration ? 'Have an account?' : 'Don`t have an account?'}
+          <button
+            className={styles.modeButton}
+            type="button"
+            onClick={() => { setIsRegistration((prev) => !prev); }}
+          >
+            {isRegistration ? 'log in' : 'sign up'}
+          </button>
+        </span>
+      </form>
+    </div>
+  );
+};
 
 export default AuthForm;
