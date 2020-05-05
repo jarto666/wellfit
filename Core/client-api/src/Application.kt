0db617d5a385f389abcbebd60b19e85fc79237c4
ilya.livshits <jarto666@gmail.com>
2020-05-05 23:33:01 +0300

    Authentication implemented

--- Core/client-api/src/Application.kt
+++ Core/client-api/src/Application.kt
@@ -1,14 +1,27 @@
 package com.wellfit.client.api
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.features.*
-import io.ktor.routing.*
-import io.ktor.http.*
-import io.ktor.auth.*
-import com.fasterxml.jackson.databind.*
-import io.ktor.jackson.*
+import com.auth0.jwk.JwkProviderBuilder
+import com.fasterxml.jackson.databind.SerializationFeature
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.auth.Authentication
+import io.ktor.auth.authenticate
+import io.ktor.auth.jwt.JWTPrincipal
+import io.ktor.auth.jwt.jwt
+import io.ktor.features.CORS
+import io.ktor.features.ContentNegotiation
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
+import io.ktor.http.content.default
+import io.ktor.http.content.static
+import io.ktor.jackson.jackson
+import io.ktor.response.respond
+import io.ktor.routing.get
+import io.ktor.routing.route
+import io.ktor.routing.routing
+import java.util.concurrent.TimeUnit
+
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -21,12 +34,25 @@ fun Application.module(testing: Boolean = false) {
         method(HttpMethod.Delete)
         method(HttpMethod.Patch)
         header(HttpHeaders.Authorization)
-        header("MyCustomHeader")
         allowCredentials = true
         anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
     }
 
+    val jwtAudience = environment.config.property("jwt.audience").getString()
+    val jwkIssuer = environment.config.property("jwt.domain").getString()
+
+    val jwkProvider = JwkProviderBuilder(jwkIssuer)
+        .cached(10, 24, TimeUnit.HOURS)
+        .rateLimited(10, 1, TimeUnit.MINUTES)
+        .build()
+
     install(Authentication) {
+        jwt {
+            verifier(jwkProvider, jwkIssuer)
+            validate { credential ->
+                if (credential.payload.audience.contains(jwtAudience)) JWTPrincipal(credential.payload) else null
+            }
+        }
     }
 
     install(ContentNegotiation) {
@@ -36,12 +62,22 @@ fun Application.module(testing: Boolean = false) {
     }
 
     routing {
-        get("/") {
-            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        static("/") {
+            default("index.html")
         }
 
         get("/json/jackson") {
             call.respond(mapOf("hello" to "world"))
+        }
+
+        authenticate() {
+            route("/graphql") {
+                get {
+                    call.respond("Sheet")
+//                    val user = call.authentication.principal<JwtTokenManager.TokenValidity.Valid>()
+//                        ?: JwtTokenManager.TokenValidity.NotValid
+                }
+            }
         }
     }
 }
